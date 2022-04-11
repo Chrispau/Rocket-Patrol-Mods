978056d971ccf52ca9400c1ca77904755aff64f9
Chrispau <35931234+Chrispau@users.noreply.github.com>
2022-04-11 16:56:22 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,7 +4,7 @@ class Play extends Phaser.Scene {
     }
     preload() {
         // load images/tile sprites
-        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('rocket', './assets/rocket.PNG');
         this.load.image('spaceship', './assets/spaceship.PNG');
         this.load.image('starfield', './assets/starfield.PNG');
         // load spritesheet
