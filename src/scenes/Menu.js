b831799d976071043c0ace8dd359083799821e16
Chrispau <35931234+Chrispau@users.noreply.github.com>
2022-04-15 18:49:35 -0700

    menu changes
    music and colors

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,20 +7,23 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.audio('Bach', './assets/Bach.mp3');
     }
     create() {
         let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top:5,
-                bottom:5,
-            },
-            fixedWidth: 0
-        }
+          fontFamily: 'Verdana',
+          fontSize: '28px',
+          backgroundColor: '#E8D547',
+          color: '#843605',
+          align: 'right',
+          padding: {
+              top: 5,
+              bottom: 5,
+              right:5,
+              left:5,
+          },
+          fixedWidth: 0
+      }
     
       this.add.text(game.config.width/2, game.config.height/3 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
       this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
