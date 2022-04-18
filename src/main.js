6af8a1f0439d968e263123b9ddd8f60d76eb7721
Chrispau <35931234+Chrispau@users.noreply.github.com>
2022-04-18 00:49:05 -0700

    added points

--- src/main.js
+++ src/main.js
@@ -10,3 +10,16 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
+
+
+/// New UFO enemy      +20
+/// Added Timer        +10
+/// +1 sec for hit     +20
+/// new Title Screen   +10
+/// Background music   +5
+/// Scroll background  +5
+
+/// removed borders    +5 (not new artwork)
+/// redesign assests   +15 (spaceships and rocket, but not explosions)
+
+/// total should be 90 points
