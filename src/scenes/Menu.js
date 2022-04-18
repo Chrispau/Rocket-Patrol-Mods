55245032036637e3708ce390e926dc5fa1273fbe
Chrispau <35931234+Chrispau@users.noreply.github.com>
2022-04-17 21:54:01 -0700

    Title Screen change

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,20 +1,24 @@
 class Menu extends Phaser.Scene {
-    constructor() {
-        super("menuScene");
-    }
-    preload() {
-        // load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', './assets/explosion38.wav');
-        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
-        this.load.audio('Bach', './assets/Bach.mp3');
-    }
-    create() {
-        let menuConfig = {
+  constructor() {
+      super("menuScene");
+  }
+
+  preload() {
+      // load audio
+      this.load.audio('sfx_select', './assets/blip_select12.wav');
+      this.load.audio('sfx_explosion', './assets/explosion38.wav');
+      this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+      this.load.audio('Bach', './assets/Bach.mp3');
+      this.load.image('planets', './assets/planets.png');
+  }
+
+  create() {
+      // menu text configuration
+      let menuConfig = {
           fontFamily: 'Verdana',
           fontSize: '28px',
-          backgroundColor: '#E8D547',
-          color: '#843605',
+          backgroundColor: '#BF4D34',
+          color: '#000',
           align: 'right',
           padding: {
               top: 5,
@@ -24,35 +28,37 @@ class Menu extends Phaser.Scene {
           },
           fixedWidth: 0
       }
-    
-      this.add.text(game.config.width/2, game.config.height/3 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-      this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-      menuConfig.backgroundColor = '#47C8E8';
-      menuConfig.color = '#000';
-      this.add.text(game.config.width/2, game.config.height/2 + borderUISize + 2*borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+      
+      // show menu text
+      let bg = this.add.image(240,240,'planets');
+      this.add.text(game.config.width/2, game.config.height/4 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height-40, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height/2 + 3*borderUISize + 4*borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
       this.sound.play('Bach');
-        // define keys
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    }
-    update() {
-        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-          // easy mode
-          game.settings = {
-            spaceshipSpeed: 3,
-            gameTimer: 60000    
-          }
-          this.sound.play('sfx_select');
-          this.scene.start('playScene');    
+      // define keys
+      keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+      keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+  }
+
+  update() {
+      if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+        // Novice mode
+        game.settings = {
+          spaceshipSpeed: 3,
+          gameTimer: 60000    
         }
-        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
-          // hard mode
-          game.settings = {
-            spaceshipSpeed: 4,
-            gameTimer: 60000    
-          }
-          this.sound.play('sfx_select');
-          this.scene.start('playScene');    
+       
+        this.sound.play('sfx_select');
+        this.scene.start("playScene");    
+      }
+      if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+        // Expert mode
+        game.settings = {
+          spaceshipSpeed: 4,
+          gameTimer: 60000    
         }
+        this.sound.play('sfx_select');
+        this.scene.start("playScene");    
+      }
     }
 }
